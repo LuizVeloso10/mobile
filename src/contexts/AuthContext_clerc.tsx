8caae3d57ed8d9543d872bba3762f19877ac4f2b
COSTELA VELOSO <luiz.jveloso@gmail.com>
2024-09-05 15:42:32 -0300

    context auth session

--- src/contexts/AuthContext_clerc.tsx
+++ src/contexts/AuthContext_clerc.tsx
@@ -2,8 +2,10 @@ import {createContext, ReactNode, useState, useEffect}  from 'react'
 import * as Google from 'expo-auth-session/providers/google'
 import * as AuthSession from 'expo-auth-session';
 import * as WebBrowser from 'expo-web-browser'
+import {ClerkProvider, useOAuth, useUser} from "@clerk/clerk-expo"
 import {useAuth as useAuthClerk} from "@clerk/clerk-expo"
-
+import * as Linking from 'expo-linking';
+import { useAuth} from '../hooks/useAuth';
 
 
 
@@ -34,8 +36,7 @@ export function AuthContextProvider({children}: AuthProviderProps){
     const [userLogin, setUser] = useState<UserProps>({} as UserProps);
     const [isUserLoading, setIsUserLoading] = useState(false);
     const {isSignedIn, isLoaded, getToken} = useAuthClerk()
-    
-    console.log(AuthSession.makeRedirectUri())
+    const {user} = useUser()
 
     const [request, response, promptAsync] = Google.useAuthRequest({
         clientId: "902095196030-81ojvh6vmcvtbkru0qr0jp3ghogr5ltm.apps.googleusercontent.com",
@@ -54,7 +55,7 @@ export function AuthContextProvider({children}: AuthProviderProps){
       useEffect(() => {
         if (isSignedIn && isLoaded) {
             // Pega os dados do usuário
-         
+            fetchUserData();
         }
     }, [isSignedIn, isLoaded]);
 
@@ -66,10 +67,34 @@ export function AuthContextProvider({children}: AuthProviderProps){
         }
       }, [response]);
    
-   
+    const googleOAuth = useOAuth({strategy: "oauth_google"}) 
     async function signIn(){
 
-      
+      try {
+        setIsUserLoading(true);
+        const redirectURL = Linking.createURL("/")
+        const oAuthFlow = await googleOAuth.startOAuthFlow({redirectUrl: redirectURL});
+        if(oAuthFlow.authSessionResult?.type === "success"){
+          if(oAuthFlow.setActive){
+            await oAuthFlow.setActive({
+              session: oAuthFlow.createdSessionId
+             
+            
+            })
+
+          
+          }
+        }else{
+          setIsUserLoading(false)
+        }
+
+      } catch (error) {
+        setIsUserLoading(false);
+      }
+   
+
+
+      /*
         try {
             setIsUserLoading(true);
             if (request) { // Verifique se o request está pronto
@@ -82,10 +107,25 @@ export function AuthContextProvider({children}: AuthProviderProps){
           } finally {
             setIsUserLoading(false);
           }
-            
+            */
     }
 
-   
+    async function fetchUserData() {
+        try {
+            const token = await getToken();
+            console.log("Token de autenticação:", token);
+
+            const userData: UserProps = {
+                name: user?.fullName || "Usuário",
+                avatarUrl: user?.imageUrl || "",
+            };
+            setUser(userData);
+
+            // Você pode usar o token para fazer chamadas à API aqui
+        } catch (error) {
+            console.error("Erro ao obter o token:", error);
+        }
+    }
 
     return(
    
